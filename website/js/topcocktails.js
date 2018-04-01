c8bf83deddf2584e4975f8005f0c96fbef5c6e0d
ChrisGarratt122 <1704899@rgu.ac.uk>
2018-04-01 16:27:29 +0100

    First commit to new Back End Branch. Structure changed, started top.js.

---
+++ website/js/topcocktails.js
@@ -0,0 +1,29 @@
+//Function for page startup
+$(function(){
+  //alert("document ready");
+  //Get Info for first top pick from API
+  //Test alert
+  alert("Getting 'Mojito from API'")
+  var data1 = getResultFromCoctailDB("Mojito");
+  //Test alert
+  alert("Sending data to print function");
+  printJSON(data1, "#cocktailDesc1");
+});
+
+//Function gets data from theCocktailDB API
+function getResultFromCocktailDB(searchterms) {
+  //call cocktail API using Ajax
+  //build url for the request
+  var url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchterms;
+  //use jquery json shortcut
+  $.getJSON(url, function(jsondata) {
+    //handle the results
+    return jsondata;
+  });
+}
+
+function printJSON(jsondata, target){
+  //prints the JSON to the screen
+  var normal = JSON.stringify(jsondata);
+  $(target).append("<p>" + normal + "</p>");
+}
