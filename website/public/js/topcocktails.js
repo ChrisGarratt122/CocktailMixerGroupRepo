497dddb0c9abeaa9a9b6bca53eba10c83f40339d
ChrisGarratt122 <1704899@rgu.ac.uk>
2018-04-02 14:56:52 +0100

    Cleaning topcocktails.js code

--- website/public/js/topcocktails.js
+++ website/public/js/topcocktails.js
@@ -1,13 +1,17 @@
 //Function for page startup
 $(function(){
   //alert("document ready");
+
   //Get Info for first top pick from API
+
   //Test alert
   //alert("Getting 'Mojito' from API")
-  //var data1 = getResultFromCocktailDB("Mojito");
+
   getResultFromCocktailDB("Mojito");
+
   //Test alert
   //alert("Sending data to print function");
+  
   printJSON(data1, "#cocktailDesc1");
 });
 
@@ -15,18 +19,22 @@ $(function(){
 function getResultFromCocktailDB(searchterms) {
   //Test alert telling that function has been entered
   //alert("getResultFromCocktailDB entered");
+
   //call cocktail API using Ajax
   //build url for the request
   var url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchterms;
+
   //Test alert to display url variable
   //alert("Url built: " + url);
+
   //use jquery json shortcut
   $.getJSON(url, function(jsondata) {
     //Test alert to show getJSON function has been entered
     //alert("getJson function has been entered");
-    //handle the results
+
+    //Creating target variable to hold what element end result will be printed in.
     var target = "#cocktailDesc1";
-    //printJSON(jsondata, target);
+    //Send jsondata and target to printing function
     printDescription(jsondata, target);
   });
 }
@@ -38,30 +46,16 @@ function printDescription(jsondata, target){
   //Creating string to hold HTML code (Desc from API) that will be injected.
   var descstring = "";
 
-  //Locate description in result
-  //for (var i=0; i<10; i++){
     //Test alert to show for has been entered
     //alert("desc about to be created");
+
+    //Searching jsondata for instructions and creating desc varaible to hold it
     var desc = jsondata.drinks[0].strInstructions;
     descstring += desc;
-  //}
 
   //Test alert to display descstring
-  alert("Descstring= " + descstring);
+  //alert("Descstring= " + descstring);
 
   //Append descstring to desc box
   $(target).append(descstring);
 }
-
-// function printJSON(jsondata, target){
-//   //Test alert telling that function has been entered
-//   alert("printJSON entered");
-//
-//   //prints the JSON to the screen
-//   //var normal = JSON.stringify(jsondata);
-//   var pretty = JSON.stringify(jsondata, null, 4);
-//   //$(target).append("<p>" + normal + "</p>");
-//   $(target).append("<pre>" + pretty + "</pre>");
-//
-//   //Test alert telling that stringify has happened
-//   alert("Stringify has happened.");
