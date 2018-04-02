9dbc0c0e8757506b8bc325bc71a6cc587dfb58b8
ChrisGarratt122 <1704899@rgu.ac.uk>
2018-04-02 10:54:22 +0100

    Adding comments to server.js, changing and adding constants.

--- website/server.js
+++ website/server.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var session = require('express-session');
-var app = express();
+const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
+const url = "mongodb://localhost:27017/profiles";
+const express = require('express'); //npm install express
+const session = require('express-session'); //npm install express-session
+const bodyParser = require('body-parser'); //npm install body-parser
+const app = express();
 
+// var express = require('express');
+// var session = require('express-session');
+// var app = express();
+
+//Setting View Engine to ejs
 app.set('view engine', 'ejs');
 
+//******GET ROUTES (Displaying Pages)*******
 app.get('/', function(req, res) {
   res.render('pages/index');
 });
@@ -20,6 +29,7 @@ app.get('/mycocktails', function(req, res) {
   res.render('pages/myCocktails');
 });
 
+//*******Telling server to listen on port 8080*****
 app.listen(8080);
 
 
