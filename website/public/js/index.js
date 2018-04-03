60fccea5994d933541c51c567268bde8aae9d851
ChrisGarratt122 <1704899@rgu.ac.uk>
2018-04-03 12:11:49 +0100

    Trying to include partials from index.js

---
+++ website/public/js/index.js
@@ -0,0 +1,20 @@
+$(function(){
+  //alert("document ready");
+
+  //Get Info for first top pick from API
+
+  //Test alert
+  //alert("Getting 'Mojito' from API")
+
+  // getResultFromCocktailDB("Mojito", "1");
+  // getResultFromCocktailDB("Margarita", "2");
+  // getResultFromCocktailDB("Black_Russian", "3");
+  // getResultFromCocktailDB("Black_Forest_Shake", "4");
+
+  document.getElementById("bordercontainer").innerHTML=
+  <%- include("../partials/cocktailbox",{name1:"MojitoTest1",name2:"MargaritaTest1",name3:"Black RussianTest1"}) %>
+  <%- include("../partials/cocktailbox",{name1:"MojitoTest2",name2:"MargaritaTest2",name3:"Black RussianTest2"}) %>
+  <%- include("../partials/cocktailbox",{name1:"MojitoTest3",name2:"MargaritaTest3",name3:"Black RussianTest3"}) %>;
+
+
+});
