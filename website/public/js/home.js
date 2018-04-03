b7a2b427e12e0e7e508a6f81c9197c5ec29d9938
ChrisGarratt122 <1704899@rgu.ac.uk>
2018-04-03 13:41:01 +0100

    Added alerts to home.js

--- website/public/js/home.js
+++ website/public/js/home.js
@@ -11,12 +11,34 @@ $(function(){
   // getResultFromCocktailDB("Black_Russian", "3");
   // getResultFromCocktailDB("Black_Forest_Shake", "4");
 
-  alert("Function entered, about to attempt html insert");
+  alert("Function entered, about to attempt row append");
 
-  document.getElementById("bordercontainer").innerHTML= <%- include('../partials/cocktailbox',{name1:'MojitoTest1',name2:'MargaritaTest1',name3:'Black RussianTest1'}) %>;;
+  appendCocktailRow();
+
+  alert("About to attempt box append");
+
+  appendCocktailBox();
+
+  //document.getElementById("bordercontainer").appendChild=
   // <%- include("../partials/cocktailbox",{name1:"MojitoTest1",name2:"MargaritaTest1",name3:"Black RussianTest1"}) %>
   // <%- include("../partials/cocktailbox",{name1:"MojitoTest2",name2:"MargaritaTest2",name3:"Black RussianTest2"}) %>
   // <%- include("../partials/cocktailbox",{name1:"MojitoTest3",name2:"MargaritaTest3",name3:"Black RussianTest3"}) %>;
 
 
 });
+
+function appendCocktailRow() {
+
+  alert("appendCocktailRow entered.")
+  var htmlstring = "<div class='row text-center'></div;"
+
+  $("#bordercontainer").append(htmlstring);
+
+}
+
+function appendCocktailBox() {
+
+  alert("appendCocktailBox entered.")
+  var htmlstring = "";
+
+}
