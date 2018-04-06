3dc85176e3af369a1bf377be7466c16c0d0f5f3e
ChrisGarratt122 <1704899@rgu.ac.uk>
2018-04-06 15:32:24 +0100

    Adding bracket to fix each function

--- website/public/js/home.js
+++ website/public/js/home.js
@@ -295,10 +295,10 @@ function getDrinksFromCocktailDB() {
 function displayCocktails(array) {
   //Basic test version
   var text = "";
-  $(array).each(function) {
+  $(array).each(function {
     console.log($(this).text());
     text += $(this).text() + "/n";
-  }
+  )};
 
   $("#bordercontainer").append(text);
 }
