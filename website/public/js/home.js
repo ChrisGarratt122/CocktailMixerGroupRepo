a566868cb712e699707268b2b5eced8eea44bc7f
ChrisGarratt122 <chris.garratt62@btinternet.com>
2018-04-15 17:25:13 +0100

    Adding text appendage for final row end

--- website/public/js/home.js
+++ website/public/js/home.js
@@ -308,6 +308,7 @@ function displayCocktails(printArray) {
     if (index === (length - 1) && index != 0) {
         tempStr = "";
         tempStr += appendCocktailRowEnd(text);
+        text += tempStr;
         console.log("Appending final row end.");
     } else {
       if (counter == 0) {
