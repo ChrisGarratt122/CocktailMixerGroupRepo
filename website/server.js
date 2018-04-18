2e03ffb4f52e37f445cef72808cea4e4f83daf15
ChrisGarratt122 <chris.garratt62@btinternet.com>
2018-04-18 19:50:17 +0100

    Changed alerts to console logs

--- website/server.js
+++ website/server.js
@@ -81,14 +81,14 @@ app.get('/signup', function(req, res) {
 //the dologin route detasl with the data from the login screen.
 //the post variables, username and password ceom from the form on the login page.
 app.post('/dologin', function(req, res) {
-  alert(JSON.stringify(req.body));
+  console.log(JSON.stringify(req.body))
   var uname = req.body.username;
   var pword = req.body.password;
 
   db.collection('profiles').findOne({"username":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
-    if(!result){alert("There was no result!");}
+    if(!result){console.log("There was no result!");}
     //if(!result){res.redirect('/login');return}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
     if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/') }
