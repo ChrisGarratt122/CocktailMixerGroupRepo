4050852c58083d836d2c64d5dbcdeaee2cdc7afb
ChrisGarratt122 <chris.garratt62@btinternet.com>
2018-04-21 16:34:01 +0100

    UNDO RETURN UNDO UNDO

--- website/public/js/home.js
+++ website/public/js/home.js
@@ -296,7 +296,6 @@ function getDrinksFromCocktailDB() {
                 var newArray = printArray.concat(currentArray);
                 printArray = newArray;
                 console.log("New print array:" + printArray);
-                return printArray;
               }
             }
 
