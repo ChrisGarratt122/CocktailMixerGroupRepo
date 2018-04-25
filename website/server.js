489c03fd308a8d869b697f11ec9e166f85f3292f
ChrisGarratt122 <chris.garratt62@btinternet.com>
2018-04-25 20:37:03 +0100

    Added no match messages

--- website/server.js
+++ website/server.js
@@ -144,7 +144,7 @@ app.post('/dologin', function(req, res) {
   var pword = req.body.password;
 
   if (req.body.username == '' || req.body.password == '') {
-    res.render('pages/login', {message: "One of the fields were empty! Please try again."});
+    res.render('pages/login', {message: "One of the fields was empty! Please try again."});
     return;
   }
 
@@ -157,7 +157,7 @@ app.post('/dologin', function(req, res) {
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){
       console.log("There was no result!");
-      res.redirect('/login');
+      res.redirect('/login', {message: "No match! Please try again."});
       return
     }
     //if(!result){res.redirect('/login');return}
@@ -167,7 +167,7 @@ app.post('/dologin', function(req, res) {
       req.session.loggedin = true; res.redirect('/')
     }
     //otherwise send them back to login
-    else{res.redirect('/login')}
+    else{res.redirect('/login'), {message: "No match! Please try again."}}
     console.log("There was a result, but the password did not match! Re-directing back to login.");
   });
 });
