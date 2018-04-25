26e5cc911031a0a2b165b3e36d4aeed8573e3d1c
ChrisGarratt122 <chris.garratt62@btinternet.com>
2018-04-25 12:58:12 +0100

    Added display for
    Trying profile data call with appends

---
+++ website/public/js/profile.js
@@ -0,0 +1,27 @@
+$(function(){
+  $.ajax({
+    type: 'GET',
+    url: '/saveddrinks',
+    dataType: "json",
+    cache: false,
+    contentType: "application/json",
+    success: function(data) {
+      if (data) {
+        console.log("Username: " + data.login.username);
+        console.log("E-mail: " + data.email);
+        console.log("[MAKE SURE THIS IS DELETED] Password: " + data.login.password);
+        var uname = data.login.username;
+        var email = data.email;
+        var password = data.login.password;
+
+        console.log("Appending to form");
+        $("#inputEmail").append(text);
+        $("#inputName").append(text);
+        $("#inputPassword").append(text);
+        $("#inputConPassword").append(text);
+
+      }
+      else {
+        console.log("No data");
+      }
+}
